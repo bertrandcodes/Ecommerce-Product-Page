83bba7d259c964d21363dc5823d6e7273ce35e72
Bertrand Shao <shaob@g.ucla.edu>
2020-10-20 16:25:10 -0700

    feat: set up product component

---
+++ product-app/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <main id="app">
+    <section class="products">
+      <Products />
+    </section>
+  </main>
+</template>
+
+<script>
+import productData from "../catalog.json";
+import Products from "./components/Products.vue";
+export default {
+  name: "App",
+  components: { Products },
+  data() {
+    return {
+      products: productData.products,
+    };
+  },
+};
+</script>
+
+<style></style>

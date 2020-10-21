efdd3b6aceff5f85a6fb375799d8acbb32220f14
Bertrand Shao <shaob@g.ucla.edu>
2020-10-20 18:26:24 -0700

    feat: set up product page and create icons file for svgs

--- product-app/src/App.vue
+++ product-app/src/App.vue
@@ -1,17 +1,35 @@
 <template>
   <main id="app">
-    <section class="products">
-      <Products />
+    <section class="app-wrap">
+      <div class="app-header">
+        <div class="logo" />
+        <div class="cart-container">
+          <Icon name="cart"></Icon>
+          <div class="cart-total">
+            <div class="cart-num">1</div>
+          </div>
+        </div>
+      </div>
+      <div class="collection-list">
+        <Products
+          v-for="product in products"
+          :key="product.id"
+          :product="product"
+        />
+      </div>
     </section>
   </main>
 </template>
 
 <script>
+import "./assets/App.css";
 import productData from "../catalog.json";
 import Products from "./components/Products.vue";
+import Icon from "./components/Icon.vue";
+
 export default {
   name: "App",
-  components: { Products },
+  components: { Products, Icon },
   data() {
     return {
       products: productData.products,
--- product-app/src/components/Products.vue
+++ product-app/src/components/Products.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="product">
-        Product
-    </div>
+  <div class="product">
+    Product
+  </div>
 </template>
